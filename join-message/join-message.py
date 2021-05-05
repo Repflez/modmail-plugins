14a874fb9cc0d467555bbd310fd8efaba36697bd
Repflez <659133+Repflez@users.noreply.github.com>
2021-05-05 17:07:29 -0600

    Try 3

--- join-message/join-message.py
+++ join-message/join-message.py
@@ -12,22 +12,11 @@ from core.models import PermissionLevel
 class JoinMessagePlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = bot.plugin_db.get_partition(self)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        config = await self.db.find_one({"_id": "mtp-join-config"})
-
-        if config is None:
-            logger.info("User joined, but no join message was set.")
-            return
-
-        try:
-            message = config["mtp-join-message"]["message"]
-            channel = self.bot.get_channel(328236301021347841)
-            await channel.send(f"{member.mention} is taking a rest at the Inn.")
-        except:
-            return
+        channel = self.bot.get_channel(328236301021347841)
+        await channel.send(f"{member.mention} is taking a rest at the Inn.")
 
 def setup(bot):
     bot.add_cog(JoinMessagePlugin(bot))
