dfcad049e5515d9c0d153ed1c8544ddb01ad7bb9
Repflez <659133+Repflez@users.noreply.github.com>
2021-05-09 16:14:25 -0600

    Add channels to the message

--- join-message/join-message.py
+++ join-message/join-message.py
@@ -12,7 +12,7 @@ class JoinMessagePlugin(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = self.bot.get_channel(328236301021347841)
-        await channel.send(f"{member.mention} is taking a rest at the Inn.")
+        await channel.send(f"{member.mention} is taking a rest at the Inn.\nWelcome! Please check out <#813243687983251476>, <#749651917587873903> and <#618548247367122954>!")
 
 def setup(bot):
     bot.add_cog(JoinMessagePlugin(bot))
