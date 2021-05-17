b0144dcb4721d33517d1fc0eb42d1f6e2cf8730a
Repflez <659133+Repflez@users.noreply.github.com>
2021-05-16 20:31:11 -0600

    Create say.py

---
+++ say/say.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class MTPSayPlugin(commands.Cog):
+    def __init__ (self, bot):
+        self.bot = bot
+        
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def mtpsay(self, ctx, message: str):
+        """I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda."""
+        channel = self.bot.get_channel(328236301021347841)
+        await channel.send(str)
+        
+def setup(bot):
+    bot.add_cog(MTPSayPlugin(bot))
