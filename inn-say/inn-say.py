2d5463ec78cfcbc69b26d593daf9ba65a443dab2
Repflez <659133+Repflez@users.noreply.github.com>
2021-05-16 20:40:40 -0600

    Update inn-say.py

--- inn-say/inn-say.py
+++ inn-say/inn-say.py
@@ -12,7 +12,7 @@ class MTPSayPlugin(commands.Cog):
     async def mtpsay(self, ctx, message: str):
         """I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda."""
         channel = self.bot.get_channel(328236301021347841)
-        await channel.send(str)
+        await channel.send(message)
 
 def setup(bot):
     bot.add_cog(MTPSayPlugin(bot))
