807871706bc3f186443f2dae1d7d0e8d8bced2ad
Repflez <659133+Repflez@users.noreply.github.com>
2021-05-16 20:55:21 -0600

    Update inn-say.py

--- inn-say/inn-say.py
+++ inn-say/inn-say.py
@@ -12,6 +12,12 @@ class MTPSayPlugin(commands.Cog):
     async def mtpsay(self, ctx, *, message: str):
         """I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda."""
         channel = self.bot.get_channel(328236301021347841)
+        
+        try:
+            await ctx.message.delete()
+        except discord.Forbidden:
+            pass
+        
         await channel.send(f"{message}")
 
 def setup(bot):
