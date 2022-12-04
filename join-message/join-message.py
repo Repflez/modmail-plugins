98c5cb904d4215bac37a4c1b104cd3b7baf91235
Repflez <659133+Repflez@users.noreply.github.com>
2022-12-03 22:55:19 -0600

    fix: Fix join message plugin

--- join-message/join-message.py
+++ join-message/join-message.py
@@ -14,5 +14,5 @@ class JoinMessagePlugin(commands.Cog):
         channel = self.bot.get_channel(328236301021347841)
         await channel.send(f"{member.mention} is taking a rest at the Inn.\nWelcome! Please check out <#813243687983251476>, <#749651917587873903> and <#618548247367122954>!")
 
-def setup(bot):
+async def setup(bot):
     bot.add_cog(JoinMessagePlugin(bot))
