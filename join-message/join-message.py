4030884318f329d25417fb3cadccdd6a21efcefc
Repflez <repflez@tmpim.com>
2024-01-29 00:58:30 -0700

    feat: make join-message work with v4

--- join-message/join-message.py
+++ join-message/join-message.py
@@ -1,18 +1,14 @@
 import discord
 from discord.ext import commands
 
-from core import checks
-from core.models import PermissionLevel
-
-
 class JoinMessagePlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        channel = self.bot.get_channel(328236301021347841)
-        await channel.send(f"{member.mention} is taking a rest at the Inn.\nWelcome! Please check out <#813243687983251476>, <#749651917587873903> and <#618548247367122954>!")
+        channel = self.bot.get_channel(1140088471420145674)
+        await channel.send(f"{member.mention} is now resting in <#1140088471420145674>.")
 
 async def setup(bot):
-    bot.add_cog(JoinMessagePlugin(bot))
+    await bot.add_cog(JoinMessagePlugin(bot))
