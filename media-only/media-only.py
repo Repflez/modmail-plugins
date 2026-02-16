026cdc49b51a57885d2b87509d168f724608c409
Repflez <repflez@tmpim.com>
2026-02-15 18:22:53 -0700

    Refactor attachment validation logic in media-only.py
    Attempt to fix attachment counts

--- media-only/media-only.py
+++ media-only/media-only.py
@@ -33,9 +33,7 @@ class Mediaonly(commands.Cog):
                 await asyncio.sleep(5)
                 await self.delete(message, warning=None)
             elif len(message.attachments):
-                if len(message.attachments) > 1:
-                    await self.delete(message, warning=f'{message.author.mention}, send 1 emoji at a time.')
-                elif not (message.attachments[0].filename.endswith('.png') or message.attachments[0].filename.endswith('.gif') or message.attachments[0].filename.endswith('.jpeg') or message.attachments[0].filename.endswith('.jpg') or message.attachments[0].filename.endswith('.mov') or message.attachments[0].filename.endswith('.mp4')):
+                if not (message.attachments[0].filename.endswith('.png') or message.attachments[0].filename.endswith('.gif') or message.attachments[0].filename.endswith('.jpeg') or message.attachments[0].filename.endswith('.jpg') or message.attachments[0].filename.endswith('.mov') or message.attachments[0].filename.endswith('.mp4')):
                     await self.delete(message, warning=f'{message.author.mention}, only png, gif, jpg, jpeg, mov and mp4 files are allowed here 📷')
 
             else:
